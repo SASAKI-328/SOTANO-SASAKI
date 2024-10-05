261e4a11e82251b7ab392c08e60fa586527e5d5e
SASAKI-328 <ortizangel.dani@gmail.com>
2024-10-04 18:54:10 -0600

    Add files via upload

---
+++ practicas.py
@@ -0,0 +1,21 @@
+#variables y practicas casi iguales a las clases xd <3
+
+n1=int (input("Escribe un numero: "))
+n2=int (input("Escribe otro numero: "))
+
+r1=n1+n2
+r2=n1-n2
+r3=n1*n2
+
+print("Este es el resultado de la suma: ", r1)
+print("Este es el resultado de la resta: ", r2)
+print("Este es el resultado de la multiplicacion: ", r3)
+print("esto es todo gracias <3")
+
+p1=input("Escribe una palabra: ")
+p2=input("Escribe una palabra que complemente a la anterior: ")
+
+print("La palabra es: ", p1,p2)
+
+# esto es lo mejor del mundo aunque no lo crean perras#
+
---
+++ theend.py
@@ -0,0 +1,30 @@
+#hello world
+
+print('hola I LOVE YOU C.R.O <3')
+z  =  3
+x  =  2
+y  =  5
+res=  y + x
+print("El resultado es" , res)
+w=res+z
+print("Sumado z es igual a" , w)
+W=w+w
+max=W
+print("Esto es todo lo que e conseguido"   ,max)
+'''
+ESTE VA A SER EL INICIO 
+DE UN IMPERIO
+'''
+#consultar el tipo de dato
+print(type("soy dato str")) # tipo'str'
+print(type(5)) # tipo'int'
+print(type(1.5)) # tipo'float
+print(type(1+ 2j)) # tipo'float
+print(type(True)) # tipo'bool'
+print(2 + 5) 
+hi=5
+low='estoy aqui'
+print(low)
+print(hi)
+my_bool=True
+print(my_bool)
